5c48802d9d1ac694eb297ddffb514b2e99ed7b4a
IliyanTachev <iliqn.tachev@gmail.com>
2017-10-17 23:56:22 +0300

    okay

--- B_11_Iliyan_Tachev.rb
+++ B_11_Iliyan_Tachev.rb
@@ -1,9 +1,26 @@
 require 'csv'
 file=ARGV[0]
-CSV.foreach(file) do |row|
-hurl=row[4]
-p `curl -F "file=@sum.csv" #{hurl}/sums == "10.00"`
-p `curl -F "file=@sum.csv" #{hurl}/filters == "5.00"`
-#p `curl -F "file=@sum.csv" #{hurl}/ == "2.00"`
+sums_answer=630.00
+filters_answer=342.00
+intervals_answer=645.00
+CSV.foreach(file,:headers => true) do |row|
+	if row[1]!="" && row[2]!="" && row[3]!="" && row[4]!="" && row[5]!="" && row[6]!=""
+	
+sums=`curl -s -F "file=@/home/iliyantachev/new/software_engineering2017/B_11_Iliyan_Tachev.csv --max-time 5" #{row[5]}/sums`
+filters=`curl -s -F "file=@/home/iliyantachev/new/software_engineering2017/B_11_Iliyan_Tachev.csv --max-time 5" #{row[5]}/filters`
+intervals=`curl -s -F "file=@/home/iliyantachev/new/software_engineering2017/B_11_Iliyan_Tachev.csv --max-time 5" #{row[5]}/intervals`
+lin_regressions=`curl -s -F "file=@/home/iliyantachev/new/software_engineering2017/B_11_Iliyan_Tachev.csv --max-time 5" #{row[5]}/lin_regressions`
+	#p "#{lin_regressions}"
+	#p "#{lin_regressions}"
+	#p "#{lin_regressions}"
+	if sums==sums_answer && filters==filters_answer && intervals==intervals_answer
+		 p "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, 1"
+	else 
+		 p "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, 0"
+	end
 
-
+	else 	
+		p "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, 0"
+	
+	end
+end
