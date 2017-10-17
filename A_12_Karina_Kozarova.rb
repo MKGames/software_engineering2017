3adc99c2354be2ebb417dab12f3e6d33bc6d035a
Karina Kozarova <karinakozarova1303@gmail.com>
2017-10-17 18:24:48 +0300

    rm comments

--- A_12_Karina_Kozarova.rb
+++ A_12_Karina_Kozarova.rb
@@ -11,10 +11,10 @@ CSV.foreach(input_file) do |row|
 	result = 0
 	current_url = row[5]
 
-	r1 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/sums` # 90.00
-   	r2 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  # 0.003953,1.913043
-	r3 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/filters` # 150.00
-	r4 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/intervals` #85.00
+	r1 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/sums` 
+   	r2 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  
+	r3 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/filters`
+	r4 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/intervals`
 
 	if r1 == result_curl_1  && r2== result_curl_2 && r3 == result_curl_3 && r4 == result_curl_4
 		result = 1
@@ -24,5 +24,5 @@ CSV.foreach(input_file) do |row|
 	if count > 0
 		puts "#{row[1]},#{row[2]},#{row[3]},#{row[4]},#{result}\n" 
 	end
-		count+=1
+	count+=1
 end
