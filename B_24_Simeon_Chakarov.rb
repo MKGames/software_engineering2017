b60bde6c9581a36623d91703e6a21f2298bc9e3d
simeonch7 <simeonch7@gmail.com>
2017-10-17 21:37:43 +0300

    2nd Homework

--- B_24_Simeon_Chakarov.rb
+++ B_24_Simeon_Chakarov.rb
@@ -1,13 +1,20 @@
 require 'csv'
 
-	doc = ARGV[0]
+doc = CSV.read(ARGV[0])
 	
-	CSV.foreach(file) do |row|
-		hurl = row[5]
-		
-		p 'curl -F "file=@sum.csv" #{hurl}/sums' == "55.00"
-		p 'curl -F "file=@sum.csv" #{hurl}/filters' == "0.00"
-		p 'curl -F "file=@sum.csv" #{hurl}/intervals' == "0.00"
-		p 'curl -F "file=@sum.csv" #{hurl}/lin_regressions' == "0.00"
-		if()
+doc.each do |row|
+	1st_result = `curl -s --form \"file=@./B_24_Simeon_Chakarov.csv\" #{row[5]}/sums`.to_s
+	2nd_result =`curl -s --form \"file=@./B_24_Simeon_Chakarov.csv\" #{row[5]}/filters`.to_s
+   	3rd_result =`curl -s --form \"file=@./B_24_Simeon_Chakarov.csv\" #{row[5]}/intervals`.to_s
+   	4th_result = `curl -s --form \"file=@./B_24_Simeon_Chakarov.csv\" #{row[5]}/lin_regressions`.to_s
+	if row[1]!="" && row[2]!="" && row[3]!="" && row[4]!="" && row[5]!="" && row[6]!=""
+		if(1st_result== "528.00" && 
+		2nd_result== "272.00" && 
+		3rd_result == "525.00" && 
+		4th_result=="1.000000,0.000000")
+			puts "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 1"
+		else
+			puts "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 0"
+		end
 	end
+end
