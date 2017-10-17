518f831443ef450e09d00c492be12c21447e829b
root <p.milev3@gmail.com>
2017-10-17 21:25:42 +0300

    some changes

--- B_20_Peter_Milev.rb
+++ B_20_Peter_Milev.rb
@@ -6,12 +6,12 @@ NumRow = 2
 arr = CSV.read(ARGV[0])
 
 arr.each do |n|
-	sum = `curl --form "file=@/home/elsyser/11B/teh na prom/test.csv" #{n[HeroRow]}/sums`.to_s
-	filter = `curl --form "file=@/home/elsyser/11B/teh na prom/test.csv" #{n[HeroRow]}/filters`.to_s
-	interval = `curl --form "file=@/home/elsyser/11B/teh na prom/test.csv" #{n[HeroRow]}/intervals`.to_s
-	lin_regression = `curl --form "file=@/home/elsyser/11B/teh na prom/test.csv" #{n[HeroRow]}/lin_regressions`.to_s
+	sum = `curl -F "file=@/root/workspace/file.csv" #{n[HeroRow]}/sums`.to_s
+	filter = `curl -F "file=@/root/workspace/file.csv" #{n[HeroRow]}/filters`.to_s
+	interval = `curl -F "file=@/root/workspace/file.csv" #{n[HeroRow]}/intervals`.to_s
+	lin_regression = `curl -F "file=@/root/workspace/file.csv" #{n[HeroRow]}/lin_regressions`.to_s
 	if(n[1] != "" && n[2] != "" && n[3] != "" && n[4] != "" && n[5] != "" && n[6] != "")
-		if(sum == "528.00" && filter == "272.00" && interval == "525.00" && lin_regression == "1.000000,0.000000")
+		if(sum == "2567.00" && filter == "5873.00" && interval == "2562.00" && lin_regression == "8.812097,-58.187097")
 			p "#{n[ClassRow]} #{n[NumRow]} #{n[3]} #{n[4]} 1"
 		else
 			p "#{n[ClassRow]} #{n[NumRow]} #{n[3]} #{n[4]} 0"
