edfcfd51662c6e4327884b09b94aecfe6ef8e815
denishristov <22468578+denishristov@users.noreply.github.com>
2017-10-17 23:57:35 +0300

    another2

--- A_09_Denis_Hristov.rb
+++ A_09_Denis_Hristov.rb
@@ -2,7 +2,6 @@ require 'csv'
 
 file = ARGV[0]
 
-correct = 0
 a1 = "1176.00"
 a2 = "648.00"
 a3 = "1023.00"
@@ -14,9 +13,11 @@ CSV.foreach(file) do |row|
 	intervals = `curl --form \"file = @./A_09_Denis_Hristov.csv\" #{row[5]}/intervals`.to_s
 	lin_regressions = `curl --form \"file = @./A_09_Denis_Hristov.csv\" #{row[5]}/lin_regressions`.to_s
 	
+	correct = 0
+	
 	if sums == a1 && filters == a2 && intervals == a3 && lin_regressions == a4
-		hw = 1
+		correct = 1
 	end
 	
-	puts "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, #{hw}"
+	puts "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, #{correct}"
 end
