f181b1ff461b78ec091ab28d825b21b85b305c7a
July Slavchev <jul.slavhev.tues19@gmail.com>
2017-10-17 21:06:19 +0300

    adding changes

--- A_26_Yuliyan_Slavchev.rb
+++ A_26_Yuliyan_Slavchev.rb
@@ -1,1 +1,20 @@
+require 'csv'
 
+csv_file = ARGV[0]
+
+
+CSV.foreach(csv_file) do |row|
+	scores = 0
+	heroku_url = row[5]
+	
+	output_01 = `curl -F "file=@/home/elsyser/A_26_Yuliyan_Slavchev.csv" #{heroku_url}/sums` 
+	output_02 = `curl -F "file=@/home/elsyser/A_26_Yuliyan_Slavchev.csv" #{heroku_url}/filters` 
+	output_03 = `curl -F "file=@/home/elsyser/A_26_Yuliyan_Slavchev.csv" #{heroku_url}/intervals` 
+	output_04 = `curl -F "file=@/home/elsyser/A_26_Yuliyan_Slavchev.csv" #{heroku_url}/lin_regressions` 
+	if output_01 == "1275" && output_02 == "625" && output_03 == "1065" && output_04 == "1.000000,0.000000"
+		scores = 1
+	else 
+		scores = 0
+	end
+	puts "#{row[1]} #{row[3]} #{row[4]} #{scores}" 
+end
