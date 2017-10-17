b241d57a903a7cf850c5005449bf81aaa100fef1
Sasho32 <sashosasho32@gmail.com>
2017-10-17 11:34:12 -0700

    Update B_01_Aleksandar_Aleksandrov.rb

--- B_01_Aleksandar_Aleksandrov.rb
+++ B_01_Aleksandar_Aleksandrov.rb
@@ -2,11 +2,13 @@ require 'csv'
 
 file = ARGV[0]
 
-CSV.foreach(file, :headers => true) do |row|
-	hurl = row[5]
-	p row[3] + row[4]
-	p `curl -F "file=@sum.csv" #{hurl}/sums` == "55.00" &&
-	`curl -F "file=@sum.csv" #{hurl}/filters` == "0.00" &&
-	`curl -F "file=@sum.csv" #{hurl}/intervals` == "55.00" &&
-	`curl -F "file=@sum.csv" #{hurl}/lin_regressions` == "1.000000,0.000000"
+CSV.foreach(file, :headers => true, skip_blanks: true) do |row|
+	unless row[2].nil? && row[3].nil? && row[4].nil?	
+		hurl = row[5]
+ 		puts row[3] + " " + row[4] + ":"  
+		if(`curl -F "file=@sum.csv" #{hurl}/sums` == "55.00" && `curl -F "file=@sum.csv" #{hurl}/filters` == "0.00" && `curl -F "file=@sum.csv" #{hurl}/intervals` == "55.00" && `curl -F "file=@sum.csv" #{hurl}/lin_regressions` == "1.000000,0.000000")
+			puts 1
+		else puts 0
+		end
+	end
 end
