c0acfa07426bc067d6fc4fa39631890f12a89f51
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-17 22:22:25 +0300

    test commit

--- A_19_Momchil_Todorov.rb
+++ A_19_Momchil_Todorov.rb
@@ -1,10 +1,23 @@
 require 'csv'
-file = File.read(ARGV[0])
-print file.superclass
+file = ARGV[0]
 
-file_path = file.path
-CSV.foreach(file_path) do |row|
-	heroku_path = row[4]
-	r1 = `curl -F "file=@/home/elsyser/11a/tp/hw1_tp.csv "` + heroku_path
-	puts r1
-end
+check = 0
+CSV.foreach(input_file) do |row| 
+	result = 0
+	heroku_url = row[5]
+
+	r1 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/sums` 
+   	r2 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/filters`  
+	r3 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/intervals`
+	r4 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/lin_regressions`
+
+	if r1 == "528"  && r2== "256" && r3 == "525" && r4 == "1.000000,0.000000"
+		result = 1
+	else 
+		result = 0
+	end
+	if check > 0 && !heroku_url.nil?
+			puts "#{result}\n" 
+	end
+	check+=1
+end 
