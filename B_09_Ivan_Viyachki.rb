0874da3f2d360c7b9155cc4dd570d76d89e16a58
Ivan Viyachki <ivan_viyachki@abv.bg>
2017-10-17 20:40:14 +0300

    Update B_09_Ivan_Viyachki.rb

--- B_09_Ivan_Viyachki.rb
+++ B_09_Ivan_Viyachki.rb
@@ -1,16 +1,16 @@
-require "CSV"
+require "csv"
 
 student = CSV.read(ARGV[0])
 
 student.each do |row|
-t
-	sum = `curl –form "file=@ /home/Desktop/Test.csv" #{row[5]}/sums`.to_s 
-	filter = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/filter`.to_s 
-	interval = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/interval`.to_s
-	regresion = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/lin_regresion` .to_s 	
-if(sum == "528.00" && filter == "272.00" && interval == "525.00" && regresion == "1.000000,0.000000")
-	p #{row[3]} #{row[4]} 1;
-end
-	
-end 	
 
+    sum = `curl –form "file=@ /home/Desktop/Test.csv" #{row[5]}/sums 2>/dev/null`.to_s  
+        filter = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/filter 2>/dev/null`.to_s 
+        interval = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/interval 2>/dev/null`.to_s 
+        regresion = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/lin_regresion 2>/dev/null` .to_s 
+    if(sum == "528.00" && filter == "272.00" && interval == "525.00" && regresion == "1.000000,0.000000")
+        p "#{row[3]} #{row[4]} 1";
+    else
+        p 0
+    end        
+end 
