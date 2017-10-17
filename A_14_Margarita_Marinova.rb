acdaa9ba3b2e5940b471ece6d5a3dd55fd7dcedc
MagiMarinova <magi.marinowax@gmail.com>
2017-10-17 17:53:12 +0300

    Update A_14_Margarita_Marinova.rb

--- A_14_Margarita_Marinova.rb
+++ A_14_Margarita_Marinova.rb
@@ -1,21 +1,23 @@
 require 'csv'
 
-file=CSV.read(ARGV[0])
+c1=528
+c2=251.00
+c3=525
+c4="1.000000,0.000000"
 
-correctSum=528
-correctFilter=272.00
-correctInterval=525
-correctRegression="1.000000,0.000000"
-file.each do |row|
+csv_file=CSV.read(ARGV[0])
+
+csv_file.each do |row|
     if row[1]!="" && row[2]!="" && row[3]!="" && row[4]!="" && row[5]!="" && row[6]!=""
-        sum = `curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/sums`
-        filter =`curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/filters`
-        interval =`curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/intervals`
-        regression = `curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/lin_regressions`
-        if sum==correctSum && filter==correctFilter && interval == correctInterval && regression==correctRegression
-            p "#{row[3]} #{row[4]}: 1"
+        r1 = `curl -s --form \"file=@./A_14_Margarita_Marinova.csv\" #{row[5]}/sums`
+        r2 =`curl -s --form \"file=@./A_14_Margarita_Marinova.csv\" #{row[5]}/filters`
+        r3 =`curl -s --form \"file=@./A_14_Margarita_Marinova.csv\" #{row[5]}/intervals`
+        r4 = `curl -s --form \"file=@./A_14_Margarita_Marinova.csv\" #{row[5]}/lin_regressions`
+        if r1==c1 && r2==c2 && r3 ==c3 && r4==c4
+            puts "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 1"
+
+        else
+            puts "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 0"
         end
-    else
-        p "#{row[3]} #{row[4]}: 0"
     end
-end 
+end
