ebb5e9878c3467bb4ab792b8cbed67a96229afa9
DanielRusinov <daniel.rusinov00@gmail.com>
2017-10-18 12:20:44 +0300

    fixed filepath

--- B_06_Daniel_Rusinov.rb
+++ B_06_Daniel_Rusinov.rb
@@ -9,10 +9,10 @@ require 'csv'
 			result = 0
 			herourl = row[5]
 	
-			r1 =`curl -F "file=@/home/razbiva4/Desktop/sumx.csv" #{herourl}/sums 2>/dev/null`
-			r2 =`curl -F "file=@/home/razbiva4/Desktop/sumx.csv" #{herourl}/intervals 2>/dev/null`
-			r3 =`curl -F "file=@/home/razbiva4/Desktop/sumx.csv" #{herourl}/filters 2>/dev/null`
-			r4 =`curl -F "file=@/home/razbiva4/Desktop/sumx.csv" #{herourl}/lin_regressions 2>/dev/null`
+			r1 =`curl -F "file=@./B_06_Daniel_Rusinov.csv" #{herourl}/sums 2>/dev/null`
+			r2 =`curl -F "file=@./B_06_Daniel_Rusinov.csv" #{herourl}/intervals 2>/dev/null`
+			r3 =`curl -F "file=@./B_06_Daniel_Rusinov.csv" #{herourl}/filters 2>/dev/null`
+			r4 =`curl -F "file=@./B_06_Daniel_Rusinov.csv" #{herourl}/lin_regressions 2>/dev/null`
 
 			if r1 == "15.00" && r2 == "15.00" && r3 == "8.00" && r4 == "0.900000,0.300000" 
 				result = 1
