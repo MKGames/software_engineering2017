3f6bf837dc74531c7347cb8e68c58762ac12a4de
wencakisa <wencakisa@gmail.com>
2017-10-18 13:55:31 +0300

    Changed curl request timeout seconds

--- A_04_Vencislav_Tashev.rb
+++ A_04_Vencislav_Tashev.rb
@@ -8,7 +8,7 @@ end
 
 STUDENTS_DATA = ARGV[0]
 TEST_DATA = './A_04_Vencislav_Tashev.csv'
-TIMEOUT_SECONDS = 3
+TIMEOUT_SECONDS = 15
 INITIAL_CURL_REQUEST = "curl -s --form \"file=@#{TEST_DATA}\" -m #{TIMEOUT_SECONDS}"
 
 TESTS = [
