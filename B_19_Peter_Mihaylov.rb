d83ef98bc975fdefc460912b13ea89b0d5e63587
pepsm <peps.mihailov@gmail.com>
2017-10-18 11:59:45 +0300

    pray

--- B_19_Peter_Mihaylov.rb
+++ B_19_Peter_Mihaylov.rb
@@ -9,9 +9,9 @@ input_file.each do |row|
 	lin_regression = `curl -s --form "file=@/home/user/workspace/Ruby/Git/software_engineering2017/B_19_Peter_Mihaylov.csv"  #{row[5]}/lin_regressions`.to_s
 	if(row[1] != "" && row[2] != "" && row[3] != "" && row[4] != "" && row[5] != "" && row[6] != "")
 		if(sum == "528.00" && filter == "272.00" && interval == "525.00" && lin_regression == "1.000000,0.000000")
-			p "#{row[3]} #{row[4]}: 1"
+			p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 1"
 		else
-			p "#{row[3]} #{row[4]}: 0"
+			p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 0"
 		end
 	end
 end
