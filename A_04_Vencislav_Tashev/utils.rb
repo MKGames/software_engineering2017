d87843193a25c62189350aa6345da035d76956d1
wencakisa <wencakisa@gmail.com>
2017-10-19 23:03:58 +0300

    Added output file

--- A_04_Vencislav_Tashev/utils.rb
+++ A_04_Vencislav_Tashev/utils.rb
@@ -1,5 +1,10 @@
 module Constants
-  TEST_DATA = './A_04_Vencislav_Tashev.csv'
+  extn = File.extname $0
+  FILENAME = File.basename $0, extn
+
+  INPUT_FILE = "./#{FILENAME}.csv"
+  OUTPUT_FILE = "./#{FILENAME}_results.csv"
+
   TIMEOUT_SECONDS = 15
-  INITIAL_CURL_REQUEST = "curl -s --form \"file=@#{TEST_DATA}\" -m #{TIMEOUT_SECONDS}"
+  INITIAL_CURL_REQUEST = "curl -s --form \"file=@#{INPUT_FILE}\" -m #{TIMEOUT_SECONDS}"
 end
