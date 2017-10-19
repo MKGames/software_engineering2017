4894c817ab950d8f886a0226506a99ab2d626bee
wencakisa <wencakisa@gmail.com>
2017-10-19 23:06:11 +0300

    Removed TestCase#status_text and implemented it in strategies

--- A_04_Vencislav_Tashev/test_case.rb
+++ A_04_Vencislav_Tashev/test_case.rb
@@ -1,15 +1,5 @@
 require './utils'
 
-class String
-  def good
-    "#{"\u2713".encode('utf-8')} #{self.green}"
-  end
-
-  def bad
-    "#{"\u2613".encode('utf-8')} #{self.red}"
-  end
-end
-
 class TestCase
   attr_accessor :actual_result, :passed
   attr_reader :action, :expected_result
@@ -19,18 +9,6 @@ class TestCase
     @expected_result = expected_result
     @actual_result = nil
     @passed = false
-  end
-
-  def status_text
-    status_text = "Test on \"#{self.action}\""
-
-    if passed
-      status_text << ' passed'
-      status_text.good
-    else
-      status_text << ' failed'
-      status_text.bad
-    end
   end
 
   def run(url_string)
