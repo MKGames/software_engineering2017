22f27cb93b7565463411622cd53d342cb10c59fc
DanyYanev <yanev89@gmail.com>
2017-10-22 22:52:54 +0300

    Script remade.

--- A_08_Daniel_Yanev.rb
+++ A_08_Daniel_Yanev.rb
@@ -2,34 +2,45 @@ require 'csv'
 require 'net/http'
 
   f_path = ARGV[0]
-ARGV.each do|a|
-  puts "Argument: #{a}"
-  f_path = a;
+  fixture_csv = ARGV[1]
+
+#ARGV.each do|a|
+#  puts "Argument: #{a}"
+#  f_path = a;
+#end
+
+#id = CSV.read(f_path).size
+
+answer_1 = "0"
+answer_2 = "0"
+answer_3 = "0"
+answer_4 = "0"
+
+id = 0
+
+CSV.foreach(fixture_csv) do |row|
+  if(id == 0)
+    answer_1 = row[0]
+    answer_2 = row[1]
+    answer_3 = row[2]
+    answer_4 = row[3]
+    id += 1
+  end
 end
 
-id = CSV.read(f_path).size
-
-arr = Array.new(id)
-name = Array.new(id)
-id = 0
 CSV.foreach(f_path) do |row|
   url = row[5].to_s
   #url = "https://myapp-herokuk.herokuapp.com/"
-  r1 = `curl --max-time 4 -s -F "file=@./A_08_Daniel_Yanev.csv" #{url}sums`
-  r2 = `curl --max-time 4 -s -F "file=@./A_08_Daniel_Yanev.csv" #{url}filters`
-  r3 = `curl --max-time 4 -s -F "file=@./A_08_Daniel_Yanev.csv" #{url}intervals`
-  r4 = `curl --max-time 4 -s -F "file=@./A_08_Daniel_Yanev.csv" #{url}lin_regressions`
-  name[id] = row[3].to_s + ' ' + row[4].to_s
+  #if fixture_csv = ./A_08_Daniel_Yanev.csv (aka file wihtout header it works for all herokus)
+  r1 = `curl --max-time 4 -s -F "file=@fixture_csv" #{url}sums`
+  r2 = `curl --max-time 4 -s -F "file=@fixture_csv" #{url}filters`
+  r3 = `curl --max-time 4 -s -F "file=@fixture_csv" #{url}intervals`
+  r4 = `curl --max-time 4 -s -F "file=@fixture_csv" #{url}lin_regressions`
+  #name[id] = row[3].to_s + ' ' + row[4].to_s
   #puts "#{r1}, #{r2}, #{r3}, #{r4}"
-  if r1 == "666.00" && r2 == "13.00" && r3 == "645.00" && r4 == "1.000000,0.000000"
-    arr[id] = 1
+  if r1 == answer_1 && r2 == answer_2 && r3 == answer_3 && r4 == answer_4
+    puts "#{row[1]},#{row[2]},#{row[3]},#{row[4]}, 1\n"
   else
-    arr[id] = 0
+    puts "#{row[1]},#{row[2]},#{row[3]},#{row[4]}, 0\n"
   end
-  id += 1
-end
-
-for i in 0..id
-  puts "#{name[i]}: #{arr[i]}"
-  #puts "HELLO : #{arr[i]}"
 end
