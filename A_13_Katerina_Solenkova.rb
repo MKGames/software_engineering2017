af0f01cd97e575ddc4865a8ded8b17354339913d
katsolenkova888 <kat.solenkova.tues19@gmail.com>
2017-10-22 22:20:22 +0300

    Update A_13_Katerina_Solenkova.rb

--- A_13_Katerina_Solenkova.rb
+++ A_13_Katerina_Solenkova.rb
@@ -1,25 +1,31 @@
 require 'csv'
 
 csv_file = ARGV[0]
-result = 1
+fixtures = CSV.parse(File.read(ARGV[1]))
+#result = 0
 
+open("./fixtures.csv", 'w') { |f|
+    fixtures.drop(1).each do |row|
+        f.puts row.join(",")
+    end
+}
 
-CSV.foreach(csv_file) do |row|
-	if row[1]!="" && row[2]!="" && row[3]!="" && row[4]!="" && row[5]!="" && row[6]!=""
-	result = 0
-	url = row[5] 
-	r1 = `curl -F \"file=@./A_13_Katerina_Solenkova.csv\" #{url}/sums` 
-   	r4 = `curl -F \"le=@./A_13_Katerina_Solenkova.csv\" #{url}/lin_regressions`  
-	r2 = `curl -F \"file=@./A_13_Katerina_Solenkova.csv\" #{url}/filters` 
-	r3 = `curl -F \"file=@./A_13_Katerina_Solenkova.csv\" #{url}/intervals` 
-	
-	
-	if r1 == "54.00" && r2== "36.00" && r3 == "54.00" && r4 == "0.000000,2.000000"
-		result = 1
-	else 
-		result = 0
-	end
+CSV.foreach(csv_file, :headers => true) do |row|
+    if (row[1]!= nil && row[2]!= nil && row[3]!= nil && row[4]!= nil && row[5]!= nil && row[6]!= nil)
+    
+    url = row[5] 
+    r1 = `curl -s -F \"file=@./fixtures.csv\" #{url}/sums`.to_s 
+    r4 = `curl -s -F \"file=@./fixtures.csv\" #{url}/lin_regressions`.to_s  
+    r2 = `curl -s -F \"file=@./fixtures.csv\" #{url}/filters`.to_s  
+    r3 = `curl -s -F \"file=@./fixtures.csv\" #{url}/intervals`.to_s  
 
-	puts "#{row[3]},#{row[4]}: #{result}"
+    lin_reg = fixtures[0][3].to_s + ',' + fixtures[0][4].to_s
+    if (r1 == fixtures[0][0] && r2 == fixtures[0][1]  && r3 == fixtures[0][2]  && r4 == lin_reg )
+          puts "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, 1"
+    
+    else
+
+     puts "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, 0"
+    end
 end
 end
