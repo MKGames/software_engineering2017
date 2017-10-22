c9565689dfb0c666e706cfb17dc2dced97f9854d
July Slavhev <jul.slavchev.tues19@gmail.com>
2017-10-22 12:17:51 +0300

    Update A_26_Yuliyan_Slavchev.rb

--- A_26_Yuliyan_Slavchev.rb
+++ A_26_Yuliyan_Slavchev.rb
@@ -11,7 +11,7 @@ CSV.foreach(csv_file) do |row|
 	output_02 = `curl -F "file=@./A_26_Yuliyan_Slavchev.csv" #{heroku_url}/filters` 
 	output_03 = `curl -F "file=@./A_26_Yuliyan_Slavchev.csv" #{heroku_url}/intervals` 
 	output_04 = `curl -F "file=@./A_26_Yuliyan_Slavchev.csv" #{heroku_url}/lin_regressions` 
-	if output_01 == "1275" && output_02 == "625" && output_03 == "1065" && output_04 == "1.000000,0.000000"
+	if output_01 == "1275.00" && output_02 == "625.00" && output_03 == "1065.00" && output_04 == "1.000000,0.000000"
 		scores = 1
 	else 
 		scores = 0
