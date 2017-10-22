81c32ce23a7e7162ca41b6759544d50374540d8d
RobertaNetzova123 <roberta.netzova@gmail.com>
2017-10-22 15:26:00 -0700

    Update B_21_Roberta_Netzova.rb

--- B_21_Roberta_Netzova.rb
+++ B_21_Roberta_Netzova.rb
@@ -1,16 +1,16 @@
 require 'csv'
 
 file = ARGV[0]
-csv_file = ARGV[1]	
-sums = "126.00"
-filters = "40.00"
-intervals = "118.00"
-regressions = "0.014006,3.347899"
+csv_file = CSV.read(ARGV[1])	
+sums = csv_file[0][0]
+filters = csv_file[0][1]
+intervals = csv_file[0][2]
+regressions = csv_file[0][3]
 
 CSV.foreach(file, :headers => true) do |row|
     hurl = row[5]
-    filename = "file=@./#{csv_file}"
-	 if row[2] != nil && row[3] != nil && row[3] != nil && row[4] != nil && row[5] != nil && row[6] != nil 
+    filename = "file=@./B_21_Roberta_Netzova.csv"
+	 if row[1] != nil && row[2] != nil && row[3] != nil && row[4] != nil && row[5] != nil && row[6] != nil 
 
     		if (`curl -s -F #{filename} #{hurl}/sums` == sums &&
 		   `curl -s -F #{filename} #{hurl}/filters` == filters && 
