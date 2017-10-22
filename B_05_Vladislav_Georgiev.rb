151e6d26aa219811fe82bfd15263118c2bd7cf72
Vladislav <vladdislav.georgiev@gmail.com>
2017-10-22 12:22:58 -0700

    ADDED EMKO

--- B_05_Vladislav_Georgiev.rb
+++ B_05_Vladislav_Georgiev.rb
@@ -16,7 +16,7 @@ file.drop(1).each do |row|
 		filter=''
 		interval=''
 		regression=''
-		if  row[5]!=nil && row[4]!=nil && row[3]!=nil && row[2]!=nil && row[1]!=nil
+		if row[4]!=nil && row[3]!=nil && row[2]!=nil 
 			date=Date.parse(row[0].split(' ')[0]);	
 			if row[1]=~/[BbБб]/
 				row[1]="11Б"
@@ -29,25 +29,28 @@ file.drop(1).each do |row|
 				row[7]="0 (After the deadline #{row[0].split(' ')[0]})"
 				edited.push(row)
 			else
-				if row[5][-1]=="/"
-					sum= `curl --form 'file=@./#{testFile}' #{row[5]}sums --max-time 9 2>/dev/null`
-					filter= `curl --form 'file=@./#{testFile}' #{row[5]}filters --max-time 9 2>/dev/null`
-					interval= `curl --form 'file=@./#{testFile}' #{row[5]}intervals --max-time 9 2>/dev/null`
-					regression= `curl --form 'file=@./#{testFile}' #{row[5]}lin_regressions --max-time 9 2>/dev/null`
+				if row[5]!=nil
+					if row[5][-1]=="/"
+						sum= `curl --form 'file=@./#{testFile}' #{row[5]}sums --max-time 9 2>/dev/null`
+						filter= `curl --form 'file=@./#{testFile}' #{row[5]}filters --max-time 9 2>/dev/null`
+						interval= `curl --form 'file=@./#{testFile}' #{row[5]}intervals --max-time 9 2>/dev/null`
+						regression= `curl --form 'file=@./#{testFile}' #{row[5]}lin_regressions --max-time 9 2>/dev/null`
 				
-				else
-					sum= `curl --form 'file=@./#{testFile}' #{row[5]}/sums --max-time 9 2>/dev/null`
-					filter= `curl --form 'file=@./#{testFile}' #{row[5]}/filters --max-time 9 2>/dev/null`
-					interval= `curl --form 'file=@./#{testFile}' #{row[5]}/intervals --max-time 9 2>/dev/null`
-					regression= `curl --form 'file=@./#{testFile}' #{row[5]}/lin_regressions --max-time 9 2>/dev/null`
-				end 	
-				if sum==fixture[0][0] && filter==fixture[0][1] && interval==fixture[0][2] && regression==fixture[0][3]+","+fixture[0][4]
-					row[7]=1
-					
-					edited.push(row)
+					else
+						sum= `curl --form 'file=@./#{testFile}' #{row[5]}/sums --max-time 9 2>/dev/null`
+						filter= `curl --form 'file=@./#{testFile}' #{row[5]}/filters --max-time 9 2>/dev/null`
+						interval= `curl --form 'file=@./#{testFile}' #{row[5]}/intervals --max-time 9 2>/dev/null`
+						regression= `curl --form 'file=@./#{testFile}' #{row[5]}/lin_regressions --max-time 9 2>/dev/null`
+					end 	
+					if sum==fixture[0][0] && filter==fixture[0][1] && interval==fixture[0][2] && regression==fixture[0][3]+","+fixture[0][4]
+						row[7]=1
+						edited.push(row)
+					else
+						row[7]=0
+						edited.push(row)
+					end
 				else
 					row[7]=0
-					
 					edited.push(row)
 				end
 			end	
