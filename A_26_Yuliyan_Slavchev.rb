39081ac70ff8d80195ef777a7d1ebbdaa7a468a2
July Slavchev <jul.slavhev.tues19@gmail.com>
2017-10-23 01:06:01 +0300

    Adding 03_task

--- A_26_Yuliyan_Slavchev.rb
+++ A_26_Yuliyan_Slavchev.rb
@@ -5,8 +5,8 @@ fixture_csv_file = ARGV[1]
 
 	results = []
 CSV.foreach(fixture_csv_file) do |row|
-	results << row
-	end
+	results = row
+end
 CSV.foreach(csv_file) do |row|
 	scores = 0
 	heroku_url = row[5]
@@ -14,8 +14,9 @@ CSV.foreach(csv_file) do |row|
 	output_02 = `curl -s -F "file=@./A_26_Yuliyan_Slavchev.csv" #{heroku_url}/filters` 
 	output_03 = `curl -s -F "file=@./A_26_Yuliyan_Slavchev.csv" #{heroku_url}/intervals` 
 	output_04 = `curl -s -F "file=@./A_26_Yuliyan_Slavchev.csv" #{heroku_url}/lin_regressions` 
+
 	if output_01 == results[0] && output_02 == results[1] && output_03 == results[2] && output_04 == results[3]
-		scores = 1
+			scores = 1
 	else 
 		scores = 0
 	end
