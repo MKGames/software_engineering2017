3edb2babc3cb7cb77d3570927db949603a89a0ef
karinakozarova <karinakozarova1303@gmail.com>
2017-10-22 18:01:16 +0300

    fixed curl positions

--- A_12_Karina_Kozarova.rb
+++ A_12_Karina_Kozarova.rb
@@ -2,6 +2,7 @@ require 'csv'
 input_csv_file_with_links = ARGV[0]
 fixture_csv = ARGV[1]
 
+# puts fixture_csv
 result_curl_1 = 0
 result_curl_2 = 0	
 result_curl_3 = 0
@@ -33,16 +34,17 @@ CSV.foreach(input_csv_file_with_links) do |row|
 	current_url = row[5]
 
 	r1 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/sums` 
-   	r2 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  
-	r3 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/filters`
-	r4 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/intervals`
+	r2 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/filters`
+	r3 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/intervals`
+   	r4 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  
+
 
 	if r1 == result_curl_1 && r2.to_s== result_curl_2.to_s && r3.to_s == result_curl_3.to_s && r4.to_s == result_curl_4.to_s
 		result = 1
 	else 
 		result = 0
 	end
-	
+
 	if count > 0 && !current_url.nil?
 		# file.write("#{row[1]},#{row[2]},#{row[3]},#{row[4]},#{result}\n" )
 		puts "#{row[1]},#{row[2]},#{row[3]},#{row[4]},#{result}\n"
