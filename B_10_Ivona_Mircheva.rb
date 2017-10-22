c3ded9cc6bede976a13321f7a8cf69396ec8ba86
I-Mircheva <ivona1mircheva@gmail.com>
2017-10-22 23:11:24 +0300

    Prints results to console
    Qwerty did dis!

--- B_10_Ivona_Mircheva.rb
+++ B_10_Ivona_Mircheva.rb
@@ -1,17 +1,33 @@
+
+
 require 'csv' ###
 
-doc = ARGV[0]
+kids_for_sale = ARGV[0]
+check_them_all = CSV.read(ARGV[1])
 
-CSV.foreach(doc) do |row|
-  hurl = row[5]
+sums_out = check_them_all[0][0]
+filters_out = check_them_all[0][1]
+intervals_out = check_them_all[0][2]
+lin_regressions_out = check_them_all[0][3]
 
-  if(row[1] != nil)
+CSV.foreach(kids_for_sale) do |row|
+  heroku_url = row[5]
 
-    if( `curl -s -F "file=@./B_10_Ivona_Mircheva.csv" #{hurl}/sums` == "112.00" && `curl -s -F "file=@B_10_Ivona_Mircheva.csv" #{hurl}/filters` == "38.00" && `curl -s -F "file=@B_10_Ivona_Mircheva.csv" #{hurl}/intervals` == "110.00" && `curl -s -F "file=@B_10_Ivona_Mircheva.csv" #{hurl}/lin_regressions` == "0.018328,3.197581")
-      puts row[1] + ',' + row[2] + ',' + row[3] + ' ' + row[4] + ',1';
-    else
-      puts row[1] + ',' + row[2] + ',' + row[3] + ' ' + row[4] + ',0';
+  # CSV.open("./B_10_Ivona_Mircheva_results.csv", "a+") do |csv|
+  if(row[5] != nil)
+
+     if( `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/sums` == sums_out &&
+      `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/filters` == filters_out &&
+      `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/intervals` == intervals_out &&
+      `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/lin_regressions` == lin_regressions_out)
+     kid = String.new("#{row[1].strip},#{row[2].strip},#{row[3].strip},#{row[4].strip},1")
+     puts kid
+       # csv.puts kid
+     else
+      kid = row[1].strip + ',' + row[2].strip + ',' + row[3].strip + ',' + row[4].strip + ',0'
+      puts kid
+        # csv.puts kid
+      end
     end
-  end
+  # end
 end
-
