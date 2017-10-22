3a83d7d9d3f3d73011c9c29bce55fcabf345403c
simeonch7 <simeonch7@gmail.com>
2017-10-22 21:46:36 +0300

    :)

--- B_24_Simeon_Chakarov.rb
+++ B_24_Simeon_Chakarov.rb
@@ -12,13 +12,15 @@ doc.drop(1).each do |row|
 			lastName = row[4]			
 			herokUrl = row[5]			
 
-			first = `curl -s --form "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/sums`.to_s
-			second = `curl -s --form "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/filters`.to_s
-			third = `curl -s --form "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/intervals`.to_s
-			fourth = `curl -s --form "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/lin_regressions`.to_s
+			first = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/sums`.to_s
+			second = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/filters`.to_s
+			third = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/intervals`.to_s
+			fourth = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{herokUrl}/lin_regressions`.to_s
 
 			if(first == fxt[0][0] && second == fxt[0][1] && third == fxt[0][2] && 
 					fourth == fxt[0][3].to_s + ',' + fxt[0][4].to_s)
+			#if(first == "126.00" && second == "40,00" && third == "118.00" && 
+			#		fourth == "0.014006,3.347899") *acts strange
 				puts "#{grade},#{number},#{firstName},#{lastName},1"
 			else				
 				puts "#{grade},#{number},#{firstName},#{lastName},0"
