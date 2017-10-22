03ec2dd737a04e030feea7589cd2e5c0b4a46662
karinakozarova <karinakozarova1303@gmail.com>
2017-10-22 19:48:53 +0300

    add small csv

--- A_12_Karina_Kozarova.rb
+++ A_12_Karina_Kozarova.rb
@@ -1,4 +1,5 @@
 require 'csv'
+
 input_csv_file_with_links = ARGV[0]
 fixture_csv = ARGV[1]
 
@@ -7,7 +8,15 @@ result_curl_1 = 0
 result_curl_2 = 0	
 result_curl_3 = 0
 result_curl_4 = 0
+
 count = 0
+
+file = open("A_12_fixture_without_first_line.csv","a+")
+
+# writes csv into new file without FL -> A_12_fixture_without_first_line.csv
+File.readlines(fixture_csv).drop(1).each do |line| 
+  file.write(line)
+end 
 
 
 CSV.foreach(fixture_csv) do |row| 
@@ -26,20 +35,19 @@ CSV.foreach(fixture_csv) do |row|
 	# puts "#{result_curl_1} #{result_curl_2} #{result_curl_3} #{result_curl_4}"
 end
 
+
 count = 0
 # filename = "A_12_filename.txt"
 # file = open(filename, "w") 
 CSV.foreach(input_csv_file_with_links) do |row| 
 	result = 0
 	current_url = row[5]
+	r1 = `curl -s -F \"file=@./A_12_fixture_without_first_line.csv\" #{current_url}/sums`
+	r2 = `curl -s -F \"file=@./A_12_fixture_without_first_line.csv\" #{current_url}/filters`
+	r3 = `curl -s -F \"file=@./A_12_fixture_without_first_line.csv\" #{current_url}/intervals`
+   	r4 = `curl -s -F \"file=@./A_12_fixture_without_first_line.csv\" #{current_url}/lin_regressions`  
 
-	r1 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/sums` 
-	r2 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/filters`
-	r3 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/intervals`
-   	r4 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  
-
-
-	if r1 == result_curl_1 && r2.to_s== result_curl_2.to_s && r3.to_s == result_curl_3.to_s && r4.to_s == result_curl_4.to_s
+	if r1.to_s == result_curl_1.to_s && r2.to_s == result_curl_2.to_s && r3.to_s == result_curl_3.to_s && r4.to_s == result_curl_4.to_s
 		result = 1
 	else 
 		result = 0
@@ -51,6 +59,6 @@ CSV.foreach(input_csv_file_with_links) do |row|
 	end
 	count+=1
 end
-
-#file.close
+File.truncate('A_12_fixture_without_first_line.csv', 0) # make sure file is empty
+file.close
 
---
+++ new.rb
@@ -0,0 +1,3 @@
+ r1 = `curl -F \"file=@./A_12_fixture_without_first_line.csv\" https://myapp-herokuk.herokuapp.com/sums`
+ puts "Op:"
+puts r1
