7e43f551b08b63cb9c491a2063a9c56383e34b8a
Stefan Angelov <ste.angelov.tues19@gmail.com>
2017-10-22 13:44:40 -0400

    Update A_24_Stefan_Angelov.rb

--- A_24_Stefan_Angelov.rb
+++ A_24_Stefan_Angelov.rb
@@ -1,7 +1,11 @@
 require 'csv'
 file = File.new(ARGV[0])
 controllers = Array.new(4)
-controllers = ["/sums","/filters","/intervals","/lin_regressions"]
+controllers = ["sums","filters","intervals","lin_regressions"]
+csv = File.new(ARGV[1])
+csv_fixture = CSV.open(csv,'r') { |csv| csv.first}
+csv_path = File.absolute_path(csv)
+
 CSV.foreach(file) do |row|
 	tasks_done = 0
 	if row[5] == nil || row[1] == "Клас"
@@ -9,23 +13,23 @@ CSV.foreach(file) do |row|
 	end
 	for i in 0...4
 		heroku_path = row[5] + controllers[i].to_s
-		r1 = "curl -s -F \"file=@./A_24_Stefan_Angelov.csv\" " + heroku_path
+		r1 = "curl -s -F \"file=@#{csv_path}\" " + heroku_path
 		r1 = `#{r1}`
 		case controllers[i]
-		when "/sums"
-			if r1 == "529.00"
+		when "sums"
+			if r1 == csv_fixture[0].to_s
 				tasks_done += 1
 			end
-		when "/filters"
-			if r1 == "289.00"
+		when "filters"
+			if r1 == csv_fixture[1].to_s
 				tasks_done += 1
 			end
-		when "/intervals"
-			if r1 == "495.00"
+		when "intervals"
+			if r1 == csv_fixture[2].to_s
 				tasks_done += 1
 			end
-		when "/lin_regressions"
-			if r1 == "0.812500,3.125000"
+		when "lin_regressions"
+			if r1 == csv_fixture[3].to_s
 				tasks_done += 1
 			end
 		end
