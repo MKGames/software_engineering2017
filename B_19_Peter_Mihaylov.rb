1413305dcaf6127ae8391d4d36f5fc0240ca0c2b
pepsm <peps.mihailov@gmail.com>
2017-10-22 23:47:25 +0300

    bdbdbd

--- B_19_Peter_Mihaylov.rb
+++ B_19_Peter_Mihaylov.rb
@@ -1,17 +1,94 @@
-require "csv"
+require 'csv'
 
-input_file = CSV.read(ARGV[0])
+class Student
+  attr_accessor :class, :number, :fname, :lname, :result
 
-input_file.each do |row|
-	sum = `curl -s --form "file=@/home/user/workspace/Ruby/Git/software_engineering2017/B_19_Peter_Mihaylov.csv" #{row[5]}/sums`.to_s
-	filter = `curl -s --form "file=@/home/user/workspace/Ruby/Git/software_engineering2017/B_19_Peter_Mihaylov.csv"  #{row[5]}/filters`.to_s
-	interval = `curl -s --form "file=@/home/user/workspace/Ruby/Git/software_engineering2017/B_19_Peter_Mihaylov.csv"  #{row[5]}/intervals`.to_s
-	lin_regression = `curl -s --form "file=@/home/user/workspace/Ruby/Git/software_engineering2017/B_19_Peter_Mihaylov.csv"  #{row[5]}/lin_regressions`.to_s
-	if(row[1] != "" && row[2] != "" && row[3] != "" && row[4] != "" && row[5] != "" && row[6] != "")
-		if(sum == "528.00" && filter == "272.00" && interval == "525.00" && lin_regression == "1.000000,0.000000")
-			p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 1"
-		else
-			p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 0"
-		end
-	end
+  def initialize(cl, num, fname, lname, res)
+    @class, @number, @fname, @lname, @result = cl, num, fname, lname, res
+  end
+
+  def print
+    puts @class + "," + @number + "," + @fname + "," + @lname + "," + @result
+  end
 end
+
+students_file = ARGV[0]
+data_file = CSV.read(ARGV[1])
+
+result1 = data_file[0][0]
+result2 = data_file[0][1]
+result3 = data_file[0][2]
+result4 = data_file[0][3]
+
+students = []
+
+current_row = 0
+
+file_length = CSV.read(students_file).drop(1).length
+
+results_file='./B_19_Peter_Mihaylov_results.csv'
+
+CSV.foreach(students_file, headers: true, skip_blanks: true) do |row|
+  Thread.new do
+    if row[3] == nil
+      file_length -= 1
+      next
+    end
+
+    school_class = row[1].strip
+
+    if school_class.include?("Б") || school_class.include?("б") || school_class.include?("B") || school_class.include?("b")
+      school_class = "B"
+    elsif school_class.include?("А") || school_class.include?("а") || school_class.include?("A") || school_class.include?("a") 
+      school_class = "A"
+    end
+
+    number_in_class = row[2].strip
+
+    student_first_name = row[3].strip
+
+    student_last_name = row[4].strip
+
+    heroku_url = row[5]
+
+    if heroku_url == nil
+      result = "0"
+    else
+      if !heroku_url.end_with?("/")
+        heroku_url = heroku_url.strip + "/"
+      end
+
+      sum_res = `curl -F "file=@B_19_Peter_Mihaylov.csv" #{heroku_url}sums 2>/dev/null`
+      filters_res = `curl -F "file=@B_19_Peter_Mihaylov.csv" #{heroku_url}filters 2>/dev/null`
+      intervals_res = `curl -F "file=@B_19_Peter_Mihaylov.csv" #{heroku_url}intervals 2>/dev/null`
+      lin_reg_res = `curl -F "file=@B_19_Peter_Mihaylov.csv" #{heroku_url}lin_regressions 2>/dev/null`
+
+      if sum_res == result1 && filters_res == result2 && intervals_res == result3 && lin_reg_res == result4
+        result = "1"
+      else
+        result = "0"
+      end
+    end
+
+    student = Student.new(school_class, number_in_class, student_first_name, student_last_name, result)
+    students.push(student)
+
+    current_row += 1
+
+    if current_row == file_length
+      students.sort_by! { |s| [s.class.to_s, s.number.to_i] }
+
+      open(results_file, 'w') { |file|
+        file.puts "Class,Number,First name,Last name,Result"
+
+        students.each do |s|
+          file.puts "#{s.class},#{s.number},#{s.fname},#{s.lname},#{s.result}"
+          s.print
+        end
+      }
+
+      exit
+    end
+  end
+end
+sleep
