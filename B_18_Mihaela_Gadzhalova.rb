dfbcdeaab9a1c05eceeca739fff752e17331be03
Mihaela Gadzhalova <mihaelagadzhalova@gmail.com>
2017-10-23 11:34:22 +0300

    ready

--- B_18_Mihaela_Gadzhalova.rb
+++ B_18_Mihaela_Gadzhalova.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 file = ARGV[0]
-fixture = ARGV[1]
+fixture = CSV.read(ARGV[1])
 result = 0
 
 CSV.foreach(file, :headers => true) do |row|
@@ -9,13 +9,13 @@ CSV.foreach(file, :headers => true) do |row|
 	heroku_url = row[5]
 	#p row[1] + ', ' + row[2] + ', ' + row[3] + ', ' + row[4]  
 	
-	r1 = `curl -s -F "file=@./#{fixture}" #{heroku_url}/sums`
-	r2 = `curl -s -F "file=@./#{fixture}" #{heroku_url}/filters`
-	r3 = `curl -s -F "file=@./#{fixture}" #{heroku_url}/intervals`
-	r4 = `curl -s -F "file=@./#{fixture}" #{heroku_url}/lin_regressions`
+	r1 = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/sums`
+	r2 = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/filters`
+	r3 = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/intervals`
+	r4 = `curl -s -F "file=@./B_21_Roberta_Netzova.csv" #{heroku_url}/lin_regressions`
 
 	if(row[1] != nil && row[2] != nil && row[3] != nil && row[4] != nil && row[5] != nil && row[6] != nil)
-		if(r1 == "126.00" && r2 == "40.00" && r3 == "118.00" && r4 == "0.014006,3.347899")
+		if(r1 == fixture[0][0] && r2 == fixture[0][1] && r3 == fixture[0][2] && r4 == fixture[0][3])
 		
 			result = 1
 		else
