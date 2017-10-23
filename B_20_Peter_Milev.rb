62d8aebb2ca503ee30c94794d5d43fb7259fe935
Pe6oProgramista <p.milev3@gmail.com>
2017-10-24 01:56:38 +0300

    read results and test from one file

--- B_20_Peter_Milev.rb
+++ B_20_Peter_Milev.rb
@@ -4,7 +4,17 @@ HerokuRow = 5
 ClassRow = 1
 NumRow = 2
 arr = CSV.read(ARGV[0])
-test_file = ARGV[1]
+results = CSV.read(ARGV[1])
+
+open("./test_file.csv", 'w') { |file_row|
+	i = 0
+	results.each do |row|
+		if(i != 0)
+			file_row.puts row.join(',')
+		end
+		i = 1
+	end
+}
 counter = 0
 first_row = 1
 arr.each do |n|
@@ -15,11 +25,11 @@ arr.each do |n|
 		counter += 1
 		Thread.new do
 			unless n[1].nil? && n[2].nil? && n[3].nil? && n[4].nil? && n[5].nil?
-				sum = `curl -F "file=@./#{test_file}" #{n[HerokuRow]}/sums 2>/dev/null`.to_s
-				filter = `curl -F "file=@./#{test_file}" #{n[HerokuRow]}/filters 2>/dev/null`.to_s
-				interval = `curl -F "file=@./#{test_file}" #{n[HerokuRow]}/intervals 2>/dev/null`.to_s
-				lin_regression = `curl -F "file=@./#{test_file}" #{n[HerokuRow]}/lin_regressions 2>/dev/null`.to_s
-				if(sum == "126.00" && filter == "40.00" && interval == "118.00" && lin_regression == "0.014006,3.347899")
+				sum = `curl -F "file=@./test_file.csv" #{n[HerokuRow]}/sums 2>/dev/null`.to_s
+				filter = `curl -F "file=@./test_file.csv" #{n[HerokuRow]}/filters 2>/dev/null`.to_s
+				interval = `curl -F "file=@./test_file.csv" #{n[HerokuRow]}/intervals 2>/dev/null`.to_s
+				lin_regression = `curl -F "file=@./test_file.csv" #{n[HerokuRow]}/lin_regressions 2>/dev/null`.to_s
+				if(sum == results[0][0] && filter == results[0][1] && interval == results[0][2] && lin_regression == results[0][3])
 					puts "#{n[ClassRow]},#{n[NumRow]},#{n[3]},#{n[4]},1"
 				else
 					puts "#{n[ClassRow]},#{n[NumRow]},#{n[3]},#{n[4]},0"
@@ -27,9 +37,11 @@ arr.each do |n|
 			end
 			counter -= 1
 			if(counter == 0)
+				File.delete("./test_file.csv")
 				exit
 			end
 		end
 	end
 end
+#File.delete("./test_file.csv")
 sleep
