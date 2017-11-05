8f20787cfecf1b5a92b0175b7f52b550259e0c46
valeri <valeritzvetanov@abv.bg>
2017-11-05 23:37:40 +0200

    teri e nai ludiq

--- B_04_Valeri_Tzvetanov.rb
+++ B_04_Valeri_Tzvetanov.rb
@@ -1,24 +1,28 @@
 require'csv'
 
 file = ARGV[0]
+fixtures = CSV.parse(File.read(ARGV[1]))
 
-CSV.foreach(file,:headers => true) do |row|
-	hurl = row[5]
-
-	p0 = `curl -F "file=@./B_04_Valeri_Tzvetanov.csv" #{hurl}/sums --max-time 5` == "256.00"  
-	p1 = `curl -F "file=@./B_04_Valeri_Tzvetanov.csv" #{hurl}/filters --max-time 5` == "128.00"
-	p2 = `curl -F "file=@./B_04_Valeri_Tzvetanov.csv" #{hurl}/intervals --max-time 5` == "255.00"		
-	p3 = `curl -F "file=@./B_04_Valeri_Tzvetanov.csv" #{hurl}/lin_regressions --max-time 5` == "0.225806,4.645161"
-
-	
+open("./temp.csv", 'w') { |f|
+    fixtures.drop(1).each do |row|
+        f.puts row.join(",")
+    end
+}
+		CSV.foreach(file,:headers => true) do |row|
+		hurl = row[5]
+		p0 = `curl -F \"file=@./temp.csv\" #{hurl}/sums 2>/dev/null`.to_s  
+		p1 = `curl -F \"file=@./temp.csv\" #{hurl}/filters 2>/dev/null`.to_s
+		p2 = `curl -F \"file=@./temp.csv\" #{hurl}/intervals 2>/dev/null`.to_s		
+		p3 = `curl -F \"file=@./temp.csv\" #{hurl}/lin_regressions 2>/dev/null`.to_s
 		if row[0] == "" || row[1] == "" || row[2] == "" || row[3] == "" || row[4] == "" || row[5] == "" || row[6] == ""
-		p "0"
-		else			
-			if p0 && p1 && p2 && p3 
-			p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 1"
+     		p "#{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, 0"
+		else	
+			lin_reg = fixtures[0][3].to_s		
+			if (p0 == fixtures[0][0] && p1 == fixtures[0][1]  && p2 == fixtures[0][2]  && p3 == lin_reg )
+				p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 1\n"
+
 			else 
-			p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 0"
+				p "#{row[1]} #{row[2]} #{row[3]} #{row[4]}: 0\n"
 			end
 		end
-	  
-end 
+		end 
