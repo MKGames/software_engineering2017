165e55025575e0ef4708f89b56effdffcf235370
kireto <k.mitov@robopartans.com>
2017-11-06 11:05:47 +0200

    Start the app given by $1 up to the latest commit given with $2

---
+++ start_app_up_to_date.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GITHUB_URL=$1
+git clone $GITHUB_URL
+NAME=$(echo $GITHUB_URL | awk -F/ '{print $5}')
+
+cd $NAME
+COMMIT=$(git log --before="$2" | sed -n 1p | awk -F'[ ]' '{print $2}')
+echo "Commit is $COMMIT"
+
+git checkout $COMMIT
+heroku git:remote -a d2d190fef0af85c80c971
+git push heroku master -f
+firefox https://d2d190fef0af85c80c971.herokuapp.com/
+cd ..
+rm $NAME -rf
