9ba2916cac109997f4db7ca67e43160d84e29133
kireto <k.mitov@robopartans.com>
2017-11-06 11:59:35 +0200

    Adding the heroku migrate

--- start_app_up_to_date.sh
+++ start_app_up_to_date.sh
@@ -11,6 +11,7 @@ echo "Commit is $COMMIT"
 git checkout $COMMIT
 heroku git:remote -a d2d190fef0af85c80c971
 git push heroku master -f
+heroku rake db:migrate
 firefox https://d2d190fef0af85c80c971.herokuapp.com/
 cd ..
 rm $NAME -rf
