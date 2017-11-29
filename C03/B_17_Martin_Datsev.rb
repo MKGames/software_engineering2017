d1417f09beb1c37dddcd12e9b5b3c2d0d708237c
mdatsev <martin.datsev.p@gmail.com>
2017-11-29 12:36:57 +0200

    added decode

--- C03/B_17_Martin_Datsev.rb
+++ C03/B_17_Martin_Datsev.rb
@@ -4,34 +4,57 @@ class Cezar
             code = x.ord + shift
             if(code > 'z'.ord)
                 code -= 26
-            elsif(code < 'a'.ord && code > 'Z'.ord)
+            elsif(code < 'a'.ord && code > 'Z'.ord && shift > 0)
                 code -= 26
+            elsif(code < 'A'.ord)
+                code += 26
+            elsif(code < 'a'.ord && code > 'Z'.ord && shift < 0)
+                code += 26
             end
-            code.chr
+            if(x !~ /[[:alpha:]]/)
+                x
+            else
+                code.chr
+            end
         }.join("")
+    end
+    def decode(str, shift)
+        encode(str, -shift)
     end
 end
 
 describe Cezar do
     cz = Cezar.new 
-    it "should return bcd when calling with abc and 1" do
+    it "encode('abc', 1) should return 'bcd'" do
         result = cz.encode("abc", 1)
         expect(result).to eq "bcd"
     end
-    it "should return abc when calling with abc and 1" do
-        result = cz.encode("bcd", -1)
+    it "decode('bcd', 1) should return 'abc'" do
+        result = cz.decode("bcd", 1)
         expect(result).to eq "abc"
     end
-    it "should return zzz when calling with aaa and 1" do
+    it "encode('zzz', 1) should return 'aaa'" do
         result = cz.encode("zzz", 1)        
         expect(result).to eq "aaa"
     end
-    it "should return ZZZ when calling with aaa and 1" do
+    it "encode('ZZZ', 1) should return 'AAA'" do
         result = cz.encode("ZZZ", 1)        
         expect(result).to eq "AAA"
     end
-    it "should return ZzaB when calling with aaa and 1" do
+    it "encode('ZzaB', 1) should return 'AabC'" do
         result = cz.encode("ZzaB", 1)        
         expect(result).to eq "AabC"
+    end
+    it "decode('AAA', 1) should return 'ZZZ'" do
+        result = cz.decode("AAA", 1)        
+        expect(result).to eq "ZZZ"
+    end
+    it "decode('aaa', 1) should return 'zzz'" do
+        result = cz.decode("aaa", 1)        
+        expect(result).to eq "zzz"
+    end
+    it "decode('Lorem ipsum dolor sit amet.1234', 20) should return 'Filyg cjmog xifil mcn ugyn.1234'" do
+        result = cz.encode("Lorem ipsum dolor sit amet.1234", 20)        
+        expect(result).to eq "Filyg cjmog xifil mcn ugyn.1234"
     end
 end
