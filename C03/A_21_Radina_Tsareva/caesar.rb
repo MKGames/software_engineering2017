47fad5c967edc6f8ae494ebd512f0b565a58df08
Karina Kozarova <karinakozarova1303@gmail.com>
2017-11-29 14:10:56 +0200

    Update caesar.rb

--- C03/A_21_Radina_Tsareva/caesar.rb
+++ C03/A_21_Radina_Tsareva/caesar.rb
@@ -1,7 +1,21 @@
 class Caesar
 
-def crypt m,k 
-	"VWGU"
+def crypt plaintext,factor
+codepoints_array = []
+  ciphertext = ""
+
+  a_codepoint = 'a'.ord
+
+  plaintext.split('').each do |letter|
+    if letter =~ /[^a-zA-Z]/
+      codepoints_array << letter.bytes.join('').to_i
+    else
+      shifted_codepoint = letter.downcase.bytes.join('').to_i + factor
+      codepoints_array << (shifted_codepoint - a_codepoint) % 26 + a_codepoint
+    end
+  end
+  ciphertext = codepoints_array.pack 'C*'
+  ciphertext.upcase
 end
 
 end
