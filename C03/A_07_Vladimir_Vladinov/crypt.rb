1fb467b291c8ed91ae62991d17ee671a0ec39266
Hugh Mungus <jackroster222@gmail.com>
2017-12-03 23:07:46 +0200

    Fix crypt

--- C03/A_07_Vladimir_Vladinov/crypt.rb
+++ C03/A_07_Vladimir_Vladinov/crypt.rb
@@ -1,26 +1,39 @@
 class Caesar
 	def letter?(char)
-		char =~ /[A-Za-z]/
+		if(char =~ /[A-Z]/) != nil
+			return 1
+		elsif(char =~ /[a-z]/) != nil
+			retun 0
+		end
+		return -1
 	end
-
+	
 	def crypt(msg, key)
 		while(key-26 >= 0)
 			key -= 26
 		end
 		new = String.new
 		msg.each_char do |c|
-			if !letter?(c)
+			num = letter?(c)
+			if num  == -1
 				new << c
 				next
 			end
-			if c=="z"
-				new << ("a".ord+(key-1)).chr
-				next	
-			elsif c=="Z"
-				new << ("A".ord+(key-1)).chr
-				next
+			c = (c.ord + key).chr
+			if num == 1
+				if c.ord > "Z".ord
+					c = (c.ord - 26).chr
+				elsif c.ord < "A".ord
+					c = (c.ord + 26).chr
+				end
+			elsif num == 0
+				if c.ord > "z".ord
+					c = (c.ord - 26).chr
+				elsif c.ord < "a".ord
+					c = (c.ord + 26).chr
+				end
 			end
-			new << (c.ord + key).chr
+			new << c
 		end
 		new
 	end
