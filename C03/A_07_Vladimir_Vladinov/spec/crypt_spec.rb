46f28127a83e5ab10f0ec6a9403dda4938d7bfd8
Hugh Mungus <jackroster222@gmail.com>
2017-12-03 23:11:22 +0200

    fix

--- C03/A_07_Vladimir_Vladinov/spec/crypt_spec.rb
+++ C03/A_07_Vladimir_Vladinov/spec/crypt_spec.rb
@@ -6,5 +6,8 @@ describe Caesar do
 		expect(Caesar.new.crypt("te ;sT0", 2)).to eq "vg ;uV0"
 		expect(Caesar.new.crypt("Sobranie", 26)).to eq "Sobranie"
 		expect(Caesar.new.crypt("", 50)).to eq ""
+		expect(Caesar.new.crypt("xyz", -1)).to eq "wxy"
+		expect(Caesar.new.crypt("xyz", 2)).to eq "zab"
+		expect(Caesar.new.crypt("RUssian Standart", -26)).to eq "Russian Standart"
 	end
 end
