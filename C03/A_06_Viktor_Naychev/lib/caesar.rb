8e9867e4bcddf18bdab62a1c342aca55cc2e5ab5
root <vitonaychev@gmail.com>
2017-12-03 20:46:31 +0200

    It works?!

--- C03/A_06_Viktor_Naychev/lib/caesar.rb
+++ C03/A_06_Viktor_Naychev/lib/caesar.rb
@@ -1,5 +1,18 @@
 class Caesar
-    def self.crypt(m, k)
-        "VWGU"
+    def crypt(m, k)
+        k = (k % 26)
+        arr = Array.new
+        m.each_char do |ch|
+            if (ch =~ /[a-zA-Z]/) ==  nil
+                    arr << ch
+            elsif (ch.ord + k > "z".ord and (ch=~/[a-z]/) != nil) or (ch.ord + k > "Z".ord and (ch=~/[A-Z]/) != nil)
+                arr << (ch.ord + k - 26).chr
+            else
+                arr << (ch.ord + k).chr
+                
+            end
+        end
+        m = arr.join
+        m
     end
 end
