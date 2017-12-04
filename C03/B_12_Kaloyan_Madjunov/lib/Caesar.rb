4e7982236f9c27853f696e03adbd829659b9a619
Kaloyan Madjunov <k.madjunov@gmail.com>
2017-12-04 13:30:19 -0800

    Add crypt method

--- C03/B_12_Kaloyan_Madjunov/lib/Caesar.rb
+++ C03/B_12_Kaloyan_Madjunov/lib/Caesar.rb
@@ -1,5 +1,29 @@
 class Caesar
-    def crypt m,k
-        "VWGU"
+    def crypt m, k
+				crypted = ""
+
+				m.chars.map! { |b|
+					c = b.ord
+
+					if k < 0 && b.ord + k < 97 && b.ord > 90
+						c = 97 - (b.ord + k)
+						b = 123 - c
+					elsif b.ord + k > 122 && b.ord > 90
+						c = b.ord + k - 122
+						b = 96 + c
+					elsif k < 0 && b.ord + k < 65 && b.ord < 91
+						c = 65 - (b.ord + k)
+						b = 91 - c
+					elsif b.ord + k > 90 && b.ord < 91
+						c = b.ord + k - 90
+						b = 64 + c
+					else
+						b = c + k
+					end
+
+					crypted += b.chr
+				}
+
+				crypted
     end
 end
--- C03/B_12_Kaloyan_Madjunov/spec/caesar_model_spec.rb
+++ C03/B_12_Kaloyan_Madjunov/spec/caesar_model_spec.rb
@@ -5,5 +5,29 @@ RSpec.describe Caesar do
         it "adds key to string" do
             expect(Caesar.new.crypt("TUES", 2)).to eql("VWGU")
         end
+
+        it "adds key to string" do
+            expect(Caesar.new.crypt("TUES", 6)).to eql("ZAKY")
+        end
+
+        it "adds key to string" do
+            expect(Caesar.new.crypt("tues", 12)).to eql("fgqe")
+        end
+
+        it "adds key to string" do
+            expect(Caesar.new.crypt("tues", -6)).to eql("noym")
+        end
+
+        it "adds key to string" do
+            expect(Caesar.new.crypt("TUES", -6)).to eql("NOYM")
+        end
+
+        it "adds key to string" do
+            expect(Caesar.new.crypt("TUES", -12)).to eql("HISG")
+        end
+
+        it "adds key to string" do
+            expect(Caesar.new.crypt("tues", -12)).to eql("hisg")
+        end
     end
 end
