83d96277b7f315ad0e863228fec3e70beb601fbf
Peter Milev <32496132+Pe6oProgramista@users.noreply.github.com>
2018-01-24 01:46:50 +0200

    Add files via upload
    Peter Milev and Todor Dimitrov's level

---
+++ c5_javascript/B_20_Peter_Milev_27_Todor_Dimitrov.js
@@ -0,0 +1,104 @@
+function myBallDraw() {
+    var canvas = document.getElementById('ballGame');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+    }
+	
+	var score_canvas = document.getElementById('score');
+	if (score_canvas.getContext) {
+	  var score_ctx = score_canvas.getContext('2d');
+	  score_ctx.font = "30px Arial";
+	}
+ 
+    var raf;
+    var lives = 3;
+	var score = 0;
+	var bonus = 80;
+    var balls = new Array;
+   
+   function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for(var i = 0; i < balls.length; i++){   
+            balls[i].draw();
+            balls[i].x += balls[i].vx;
+            balls[i].y += balls[i].vy;
+ 
+          if(balls[i].y + balls[i].radius + balls[i].vy > canvas.height
+           || balls[i].y - balls[i].radius + balls[i].vy < 0) {
+            balls[i].vy = -balls[i].vy;
+          }
+ 
+          if(balls[i].x + balls[i].radius + balls[i].vx > canvas.width
+           || balls[i].x - balls[i].radius + balls[i].vx < 0) {
+            balls[i].vx = -balls[i].vx;
+          }
+		  /*if(balls.length > 1 && i == 1) {
+			document.getElementById('check').innerHTML = "x1 = " + balls[0].x + "\n" + "x2 = " + balls[1].x;}*/
+        }
+		score_ctx.clearRect(0, 0, score_canvas.width, score_canvas.height);
+		score_ctx.fillText("Score: " + score, 20, 30);
+		
+        raf = window.requestAnimationFrame(draw);		
+    };
+   
+    var ball = {
+      x: 100,
+      y: 100,
+      vx: 5,
+      vy: 1,
+      radius: 100,
+      curr_color: 'blue',
+      is_mouse_over: function(x,y) {
+        return ((this.x - radius) < x < (this.x + radius))
+            && ((this.y - radius) < y < (this.y + radius));
+      },
+      draw: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = this.curr_color;
+        ctx.fill();
+      }
+    };
+
+    balls.push(ball);
+	
+	var is_corect_click = 0;
+    canvas.addEventListener('click', function(e){
+        for(var i = 0; i < balls.length; i++){
+            if((e.x - 10 < balls[i].x + balls[i].radius && e.x - 10 > balls[i].x - balls[i].radius)
+            && (e.y - 10 < balls[i].y + balls[i].radius && e.y - 10 > balls[i].y - balls[i].radius)) {
+				var new_ball = balls[i];
+				balls.splice(i, 1);
+				new_ball.radius *= 0.7;
+			    balls.push(new_ball);
+                balls.push(new_ball);
+               
+                balls[balls.length - 2].x = Math.floor(Math.random() * 600) + 100;
+                balls[balls.length - 2].y = Math.floor(Math.random() * 400) + 100;
+               
+                balls[balls.length - 1].x = Math.floor(Math.random() * 600) + 100;
+                balls[balls.length - 1].y = Math.floor(Math.random() * 400) + 100;
+				is_corect_click = 1;
+				
+				if(e.x == ball.x && e.y == ball.y) {
+				score += 500;
+				} else {
+					bonus += 20;
+					score += bonus;
+				}
+				break;
+            }
+        }
+		if(!is_corect_click) {
+			lives--;
+				
+            if(lives < 1){
+                location.reload();
+                alert("You died");
+            }
+		}
+    });
+    draw();
+}
+myBallDraw();
