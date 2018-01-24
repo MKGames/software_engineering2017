c9ab40b5344161b68445d1eee64feedc396d1a88
koki <mihailovkonstantin364@gmail.com>
2018-01-24 12:23:09 +0200

    message

--- c5_javascript/B_13_Konstantin_Mihaylov_13_canvas/canvas_experiment.js
+++ c5_javascript/B_13_Konstantin_Mihaylov_13_canvas/canvas_experiment.js
@@ -111,6 +111,13 @@ var mousePosition = new Array();
 		this.draw = draw_ball;
 	}
 
+	canvas.addEventListener('click', function(e){
+		if((e.x - 10 < balls.x + balls.r && e.x - 10 > balls.x - balls.r)
+		&& (e.y - 10 < balls.y + balls.r && e.y - 10 > balls.y - balls.r)){
+			balls.radius *= .8;
+		}
+	});
+
 	function game_loop(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	if(mouseDown == true){
@@ -133,82 +140,3 @@ var mousePosition = new Array();
 	}
 	init();
 }
-/*
-function myBallDraw() {
-	var canvas = document.getElementById('tutorial');
-	if (canvas.getContext) {
-	  var ctx = canvas.getContext('2d');
-	}
-
-
-	var raf;
-	var lives = 3;
-
-	function draw() {
-
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		ball.draw();
-		ball.x += ball.vx;
-		ball.y += ball.vy;
-
-	  if(ball.y + ball.radius + ball.vy > canvas.height
-	   || ball.y - ball.radius + ball.vy < 0) {
-	  	ball.vy = -ball.vy;
-	  }
-
-	  if(ball.x + ball.radius + ball.vx > canvas.width
-	   || ball.x - ball.radius + ball.vx < 0) {
-	  	ball.vx = -ball.vx;
-	  }
-
-	  raf = window.requestAnimationFrame(draw);
-	}
-
-
-
-	var ball = {
-	  x: 100,
-	  y: 100,
-	  vx: 5,
-	  vy: 1,
-	  radius: 100,
-	  color: 'blue',
-	  is_mouse_over: function(x,y) {
-	  	// return (x -radius) < xParam < (x+radius)
-	  	// 	&& ((y-radius) < yParam < (y+radius));
-	  	return ((this.x -radius) < x < (this.x+radius))
-	  		&& ((this.y-radius) < y < (this.y+radius));
-	  },
-	  draw: function() {
-	    ctx.beginPath();
-	    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-	    ctx.closePath();
-	    ctx.fillStyle = this.color;
-	    ctx.fill();
-	  }
-	};
-
-	canvas.addEventListener('click', function(e){
-		if((e.x - 10 < ball.x + ball.radius && e.x - 10 > ball.x - ball.radius)
-		&& (e.y - 10 < ball.y + ball.radius && e.y - 10 > ball.y - ball.radius)
-			){
-			ball.radius *= .8;
-
-		} else {
-			lives--;
-		}
-
-		if(lives <= 0){
-			//blabasd.dd
-			location.reload();
-			alert("You died");
-		}
-
-	});
-
-	draw();
-}
-myBallDraw();
-*/
-
